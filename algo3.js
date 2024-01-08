5bf76cb59232496cf08e06b27bfb2b7034597dd3
baehanjae97 <baehanjae97@gmail.com>
2024-01-08 17:35:02 +0900

    🔨 백준 10872 수정

--- algo3.js
+++ algo3.js
@@ -5,7 +5,7 @@
 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
 
 리뷰 : 
-
+0인 경우가 계속 틀려서 수정했다.
 */
 
 const input = Number(
@@ -20,11 +20,9 @@ const input = Number(
  * @returns output
  */
 const answer = (input) => {
-  if (input === 0) return 0;
+  if (input === 0) return 1;
   if (input === 1) return 1;
-  if (input > 1) {
-    return console.log(input * answer(input - 1));
-  }
+  if (input > 1) return input * answer(input - 1);
 };
 
-return answer(input);
+return console.log(answer(input));
